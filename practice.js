cb1008aacb13c37727f02d337797435415184409
Marfi098776 <mabrurgotech@gmail.com>
2025-02-26 00:27:46 +0600

    added practice

--- practice.js
+++ practice.js
@@ -1,19 +1,19 @@
-var money = 1000 ;
-var orangePriceAndApplePrice = 700 ;
-var totalAndRest = money - orangePriceAndApplePrice ;
-// console.log(totalAndRest)
+// var money = 1000 ;
+// var orangePriceAndApplePrice = 700 ;
+// var totalAndRest = money - orangePriceAndApplePrice ;
+// // console.log(totalAndRest)
 
-var mathematics = (75.25) ;
-var biology = 65 ;
-var chemistry = 80 ;   
-var physics = (35.45) ;
-var bangla = (99.50) ;
-var average = (mathematics + biology + chemistry + physics + bangla) / 5 ;
-console.log(average.toFixed(2))
+// var mathematics = (75.25) ;
+// var biology = 65 ;
+// var chemistry = 80 ;   
+// var physics = (35.45) ;
+// var bangla = (99.50) ;
+// var average = (mathematics + biology + chemistry + physics + bangla) / 5 ;
+// console.log(average.toFixed(2))
 
-var number1 = 119 ;
-var number2 = 5 ;
-var remainder = number1 % number2 ;
+// var number1 = 119 ;
+// var number2 = 5 ;
+// var remainder = number1 % number2 ;
 // console.log(remainder)
 
 // const a = isNaN('11');
@@ -21,3 +21,58 @@ var remainder = number1 % number2 ;
 
 // const a = isNaN(2-10);
 // console.log(a)
+
+// const bergerPrice = 500 ;
+// const cokePrice = 30;
+
+// if(bergerPrice > 500){
+//     console.log('You can get a coke for free')
+// }
+// else{
+//     console.log(cokePrice)
+//     console.log('You have to pay 30')
+// }
+
+
+// BMI
+
+// const weight = 50 ;
+// const height = parseFloat(1.5) ;
+// const bmi = weight / (height*height)
+// // const bmi = weight / Math.pow(height,2);
+
+// if( bmi < 18.5 ){
+//     console.log('You are underweight')
+// }
+// else if(bmi >= 18.5 && bmi <=24.9){
+//     console.log('You are normal')
+// }
+// else if(bmi >= 25 && bmi <= 29.9){
+//     console.log('You are overweight')
+// }
+// else{
+//     console.log('You are obese')
+// }
+
+
+
+// Grade calculator
+
+const studentResult = 69;
+
+if(studentResult >= 90 && studentResult <= 100){
+    console.log('A')
+}
+else if(studentResult >= 80 && studentResult <= 89){
+    console.log('B')
+}
+else if(studentResult >= 70 && studentResult <= 79){
+    console.log('C')
+}
+else if(studentResult >= 60 && studentResult <= 69){
+    console.log('D')
+}
+else if(studentResult >= 0 && studentResult <= 59){
+    console.log('F')
+}
+
